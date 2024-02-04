eaffe021d40ce72df8e13dae0eba5067896c7f41
SoundOrion <107441051+SoundOrion@users.noreply.github.com>
2024-02-04 15:34:29 +0900

    Add files via upload

---
+++ OpenTelemetrySample2/WebApi/Hubs/NotificationHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApi.Hubs;
+
+[Authorize]
+public class NotificationHub : Hub<NotificationHubClient>
+{
+    public override async Task OnConnectedAsync()
+    {
+        await Clients.Client(Context.ConnectionId).ReceiveNotification(
+            $"Thank you for connecting {Context.User?.Identity?.Name}");
+
+        await base.OnConnectedAsync();
+    }
+}
+
+public interface NotificationHubClient
+{
+    Task ReceiveNotification(string message);
+}
---
+++ OpenTelemetrySample2/WebApi/ServerTimerNotifier.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WebApi.Hubs;
+
+namespace WebApi;
+
+public class ServerTimerNotifier : BackgroundService
+{
+    private readonly TimeSpan Period = TimeSpan.FromSeconds(5);
+    private readonly ILogger<ServerTimerNotifier> _logger;
+    private readonly IHubContext<NotificationHub, NotificationHubClient> _hubContext;
+
+    public ServerTimerNotifier(ILogger<ServerTimerNotifier> logger, IHubContext<NotificationHub, NotificationHubClient> hubContext)
+    {
+        _logger = logger;
+        _hubContext = hubContext;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var Timer = new PeriodicTimer(Period);
+
+        while (!stoppingToken.IsCancellationRequested &&
+            await Timer.WaitForNextTickAsync(stoppingToken))
+        {
+            var dateTime = DateTime.Now;
+
+            _logger.LogInformation("Executing {Service} {Time}", nameof(ServerTimerNotifier), dateTime);
+
+            //await _hubContext.Clients.All.ReceiveNotification($"Server time = {dateTime}");
+            await _hubContext.Clients
+                .User("anonymous")
+                .ReceiveNotification($"Server time = {dateTime}");
+        }
+    }
+}
